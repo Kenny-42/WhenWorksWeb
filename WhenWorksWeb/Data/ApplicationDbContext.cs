847384f8b3f4f274848bb5e7b0bc6b6ef0a7c458
Kenny-42 <lilkenny642@gmail.com>
2026-04-19 20:04:25 -0700

    Switch to custom ApplicationUser for Identity
    Updated ApplicationDbContext, DI setup, and _LoginPartial.cshtml to use ApplicationUser instead of IdentityUser for ASP.NET Core Identity integration.

--- WhenWorksWeb/Data/ApplicationDbContext.cs
+++ WhenWorksWeb/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WhenWorksWeb.Models;
 
 namespace WhenWorksWeb.Data
 {
-	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 	{
 	}
 }
