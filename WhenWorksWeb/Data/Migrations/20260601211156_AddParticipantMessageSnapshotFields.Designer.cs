66ef087abdfbfd0c05bfde204349da9161f09ed3
Kenny-42 <lilkenny642@gmail.com>
2026-06-01 14:41:57 -0700

    Add migrations for EventMessages snapshot fields
    Introduce SenderColor and SenderDisplayName fields to EventMessages to store participant info at message time. Make ParticipantId nullable and update foreign key constraints to preserve message history if participant data changes. Update model snapshot accordingly.

--- WhenWorksWeb/Data/Migrations/20260601211156_AddParticipantMessageSnapshotFields.Designer.cs
+++ WhenWorksWeb/Data/Migrations/20260601211156_AddParticipantMessageSnapshotFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhenWorksWeb.Data;
 
@@ -11,9 +12,11 @@ using WhenWorksWeb.Data;
 namespace WhenWorksWeb.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260601211156_AddParticipantMessageSnapshotFields")]
+    partial class AddParticipantMessageSnapshotFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -537,7 +540,7 @@ namespace WhenWorksWeb.Data.Migrations
                     b.HasOne("WhenWorksWeb.Models.Participant", "Participant")
                         .WithMany("Messages")
                         .HasForeignKey("ParticipantId")
-                        .OnDelete(DeleteBehavior.NoAction);
+                        .OnDelete(DeleteBehavior.SetNull);
 
                     b.Navigation("Event");
 
